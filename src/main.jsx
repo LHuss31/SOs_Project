234dbd3fe50f7a395ce78408f8a7426d28f10a8f
Marcelhrb <marcelhrb@gmail.com>
2025-06-11 18:11:31 -0300

    Pagina SystemCalls criada e base da pagina ja feita

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
-import './index.css'
 import LoginCadastro from './pages/LoginCadastro.jsx'
+import SystemCallsPage from './pages/SystemCalls.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
-      <LoginCadastro />
+      <SystemCallsPage />
     </BrowserRouter>
   </StrictMode>,
 )
