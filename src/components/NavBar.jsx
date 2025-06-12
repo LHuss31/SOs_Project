41745bd59a02d8f783937f483ce98e2282aa5461
LHuss31 <lucashuss37@gmail.com>
2025-06-11 23:07:05 -0300

    navBar atualizada

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -16,6 +16,7 @@ export default function NavBar() {
       <div className="navbar-links">
         <Link to="/Dashboard">Dashboard</Link>
         <Link to="/SystemCalls">System Calls</Link>
+        <Link to="/IO-bound">IoBound</Link>
         <Link to="/producer-consumer">Producer-Consumer</Link>
         <Link to="/memory-management">Memory Management</Link>
         <Link to="/Notes">Notes</Link>
