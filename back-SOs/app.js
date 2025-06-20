f568dbd0c53cab1940f3f0285eaa2b8fdaf5ce90
LHuss31 <lucasmichael@usp.br>
2025-06-20 00:45:44 -0300

    banco de dados funcional

--- back-SOs/app.js
+++ back-SOs/app.js
@@ -5,7 +5,7 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var usersRouter = require('./routes/UserRoutes');
 var authRouter = require('./routes/AuthRoutes'); // Adicione se quiser usar as rotas de autenticação
 
 var connectDB = require('./config/db');
